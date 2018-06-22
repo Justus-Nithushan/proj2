0124f0d08d79654ab0b760e8a38f8b961bcde288
Justus-Nithushan <nithujust@gmail.com>
2018-06-22 05:17:19 -0700

    file new

---
+++ 2017csc002/HelloWord.java
@@ -0,0 +1,10 @@
+public class HelloWord
+{
+	public static void main(String [] args)	
+	{
+			System.out.println("Welcome to java world");
+			System.out.print("Hi I am java");
+			System.out.print("Hi java I am");
+			System.out.print("\"Thank You java\"");
+	}
+}
---
+++ 2017csc002/harshani.java
@@ -0,0 +1,14 @@
+public class harshani
+{
+	public static void main(String [] args){
+		System.out.println("Learn\tjava");
+		System.out.println("\tthe");
+		System.out.println("Hard\tWay");
+		System.out.println("\tLearn java the\"Hard\" Way!");
+		System.out.println("Hello");
+		System.out.println("jelly");
+		System.out.println("Hard to belive,eh?");
+		System.out.println("surptised?/"abcde"/or )
+		
+	}
+}		
